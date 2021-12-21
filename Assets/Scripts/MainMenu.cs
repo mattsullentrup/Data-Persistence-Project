d48b3d64b4a01763ab67804005445b5c01353c88
mattsullentrup <86676327+mattsullentrup@users.noreply.github.com>
2021-12-20 18:58:05 -0600

    Gonna delete library
    Backup before trying to get missing files

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public InputField playerName;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
